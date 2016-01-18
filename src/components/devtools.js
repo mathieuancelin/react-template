bd9c14ca1c51a234f9cd6f669e52c77246afd159
Mathieu <mathieu.ancelin@gmail.com>
2016-01-18 12:12:47 +0100

    Do not include devtools in prod

--- src/components/devtools.js
+++ src/components/devtools.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
-
-const DevTools = createDevTools(
-   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
-     <LogMonitor theme="solarized" />
-   </DockMonitor>
-);
-
 if (process.env.NODE_ENV === 'production') {
+  console.log('[PROD_ENV] do not use Redux DevTools');
   module.exports = { DevTools: null };
 } else {
-  module.exports = { DevTools };
+  const React = require('react');
+  const createDevTools = require('redux-devtools').createDevTools;
+  const LogMonitor = require('redux-devtools-log-monitor');
+  const DockMonitor = require('redux-devtools-dock-monitor');
+  module.exports = { DevTools: createDevTools(
+     <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
+       <LogMonitor theme="solarized" />
+     </DockMonitor>
+  ) };
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
+  console.log('[PROD_ENV] do not use store with Redux DevTools');
   module.exports = require('./conf.prod');
 } else {
   module.exports = require('./conf.dev');
