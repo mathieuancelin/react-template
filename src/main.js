5aa7044c5430feaa8c536c2e3e9a2329d4e5ab27
Mathieu <mathieu.ancelin@gmail.com>
2016-01-26 10:24:46 +0100

    Fix polyfill

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { SimpleClicker } from './components/simpleclicker';
-
-require('babel-polyfill');
 
 export function init() {
   ReactDOM.render(<SimpleClicker />, document.getElementById('app'));
